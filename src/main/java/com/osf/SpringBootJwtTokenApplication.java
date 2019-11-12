03a21665e435de92d90a97a479663424385d26d9
andrey-grebinchak-osf <andrey.grebinchak@osf-global.com>
2019-11-12 14:42:08 +0200

    rename class

---
+++ src/main/java/com/osf/SpringBootJwtTokenApplication.java
@@ -0,0 +1,12 @@
+package com.osf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtTokenApplication.class, args);
+	}
+}
